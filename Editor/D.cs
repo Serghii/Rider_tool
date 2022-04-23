d9d0bb6d3897ef81f656b69aeeb0c8966ab14e6d
Serhii <s.safonov@room8studio.com>
2022-04-23 17:51:24 +0300

    init

---
+++ Editor/D.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public static class D
+{
+    /// <summary>
+    /// Ability to redirect tracepoint output to Unity console
+    /// </summary>
+    [UsedImplicitly]
+    public static T Log<T>(T s)
+    {
+        Debug.Log(s);
+        return s;
+    }
+}
